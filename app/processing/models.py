7087ef0b7670810445c0d9923a0d51301b111888
SaInekK <samesainek@gmail.com>
2023-09-28 01:40:45 +0300

    cr to lf line separators change

--- app/processing/models.py
+++ app/processing/models.py
@@ -1,34 +1,1 @@
-from django.contrib.auth import get_user_model
-from django.db import models
-
-
-class File(models.Model):
-    file = models.FileField(
-        verbose_name='Загруженный файл',
-        blank=True,
-        null=True,
-    )
-    cdn_url = models.CharField(
-        verbose_name='CDN URL',
-        blank=True,
-        null=True,
-    )
-    uploaded_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата/время загрузки',
-    )
-    file_type = models.CharField(
-        verbose_name='Тип файла',
-        blank=True,
-        null=True,
-    )
-    uploaded_by = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE,
-        null=True,
-        verbose_name='Юзер, загрузивший файл',
-    )
-    processed = models.BooleanField(
-        default=False,
-        verbose_name='Обработан ли файл',
-    )
+from django.contrib.auth import get_user_modelfrom django.db import modelsclass File(models.Model):    file = models.FileField(        verbose_name='Загруженный файл',        blank=True,        null=True,    )    cdn_url = models.CharField(        verbose_name='CDN URL',        blank=True,        null=True,    )    uploaded_at = models.DateTimeField(        auto_now_add=True,        verbose_name='Дата/время загрузки',    )    file_type = models.CharField(        verbose_name='Тип файла',        blank=True,        null=True,    )    uploaded_by = models.ForeignKey(        get_user_model(),        on_delete=models.CASCADE,        null=True,        verbose_name='Юзер, загрузивший файл',    )    processed = models.BooleanField(        default=False,        verbose_name='Обработан ли файл',    )
